31dbee33f59510611cca13625effadb5e7f2af37
Shubhamdev89 <119775828+Shubhamdev89@users.noreply.github.com>
2023-03-04 15:47:29 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 START_MESSAGE = '''**Hello, {}
-I Am Mdiskconvertor.site, Bulk Link Converter. I Can Convert Links Directly From Your ShortnerFly Account,
+I Am Mdiskconvertor.site, Bulk Link Converter. I Can Convert Links Directly From Your Mdiskconvertor Account,
     
 1. Go To 👉 https://Mdiskconvertor.site/member/tools/api  
 2. Than Copy API Key
@@ -18,11 +18,11 @@ Example:** `/api de303d5270f481aec928f39883da7b7f9a8812ac `
 **➕ Hit** 👉 /channel Command To Get Help About Adding your channel to bot.
 **➕ Hit** 👉 /footer To Get Help About Adding your Custom Footer to bot.
 
-If You Want Any **Other Shortner** Link Converter Bot Instead Of Shortnerfly than **contact** at 👉 @Cyniteofficial (all **shortners** support available.)
+If You Want Any **Other Shortner** Link Converter Bot Instead Of Shortnerfly than **contact** at 👉 @Shubhamdev64 (all **shortners** support available.)
 '''
 
 HELP_MESSAGE = '''**Hello, {}
-I Am ShortnerFly, Bulk Link Converter Bot. I Can Convert Links Directly From Your ShortnerFly Account,**
+I Am ShortnerFly, Bulk Link Converter Bot. I Can Convert Links Directly From Your Mdiskconvertor Account,**
     
 1. Go To 👉 https://Mdiskconvertor.site/member/tools/api  
 2. Than **Copy API** Key
